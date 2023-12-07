55d3ec9d800506b072f6ec980e0854caa08f119e
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-12-07 07:45:06 +0100

    works now

--- src/day7.jl
+++ src/day7.jl
@@ -18,76 +18,40 @@ function tocardnum(c)
     end
 end
 
-function fiveeq(hand)
-    if allequal(hand)
-        true, hand[1]
-    else
-        false, 0
+function find_hand_counts(hand)
+    counts = @MVector [0 for _ in 2:14]
+
+    for c in hand
+        counts[c-1] += 1
     end
+
+    counts
 end
 
-function foureq(hand)
-    if allequal(@view hand[1:4]) || allequal(@view hand[2:5])
-        true, hand[3]
-    else
-        false, 0
-    end
-end
-
-function fullhouse(hand)
-    if allequal(@view hand[1:3]) && hand[4] == hand[5] ||
-       hand[1] == hand[2] && allequal(@view hand[3:5])
-        true, (hand[1], hand[5])
-    else
-        false, (0, 0)
-    end
-end
-
-function threeq(hand)
-    if allequal(@view hand[1:3]) || allequal(@view hand[2:4]) ||
-       allequal(@view hand[3:5])
-        true, hand[3]
-    else
-        false, 0
-    end
-end
-
-function twopair(hand)
-    if hand[1] == hand[2] && (hand[3] == hand[4] || hand[4] == hand[5]) ||
-       hand[2] == hand[3] && hand[4] == hand[5]
-        true, (hand[2], hand[4])
-    else
-        false, (0, 0)
-    end
-end
-
-function onepair(hand)
-    for i in 1:4
-        if hand[i] == hand[i+1]
-            return true, hand[i]
+function find_hand_type_counts(counts)
+    if any(c == 5 for c in counts)
+        7
+    elseif any(c == 4 for c in counts)
+        6
+    elseif any(c == 3 for c in counts) && any(c == 2 for c in counts)
+        5
+    elseif any(c == 3 for c in counts)
+        4
+    elseif any(c == 2 for c in counts)
+        p1 = findfirst(==(2), counts)
+        if any(c == 2 for (i, c) in enumerate(counts) if i != p1)
+            3
+        else
+            2
         end
+    else
+        1
     end
-    false, 0
-end
-
-function highcard(hand)
-    true, maximum(hand)
-end
-
-function find_hand_type(hand)
-    t = 7
-    hand = sort(hand)
-    for f in (fiveeq, foureq, fullhouse, threeq, twopair, onepair, highcard)
-        if f(hand)[1]
-            return t
-        end
-        t -= 1
-    end
-    t
 end
 
 function is_hand_less(hand1, hand2)
-    (find_hand_type(hand1), hand1) < (find_hand_type(hand2), hand2)
+    (find_hand_type_counts(find_hand_counts(hand1)), hand1) <
+    (find_hand_type_counts(find_hand_counts(hand2)), hand2)
 end
 
 function part1()
@@ -106,4 +70,87 @@ function part1()
     sort!(hands, lt=((h1, _), (h2, _)) -> is_hand_less(h1, h2))
 
     sum(b * i for (i, (_, b)) in enumerate(hands))
+end
+
+function jokereq(c1, c2)
+    if c1 == 11 || c2 == 11
+        true
+    else
+        c1 == c2
+    end
+end
+
+function find_hand_type_J(hand)
+    counts = SVector(find_hand_counts_J(hand))
+
+    nJ = counts[1]
+    counts = setindex(counts, 0, 1)
+
+    function find_type_rec(nJ, counts)
+        if nJ == 0
+            find_hand_type_counts(counts)
+        else
+            best = find_hand_type_counts(counts)
+            for i in 2:13
+                new_counts = setindex(counts, counts[i] + 1, i)
+                best = max(best, find_type_rec(nJ - 1, new_counts))
+            end
+            best
+        end
+    end
+
+    find_type_rec(nJ, counts)
+end
+
+function is_hand_less_joker(hand1, hand2)
+    (find_hand_type_J(hand1), hand1) <
+    (find_hand_type_J(hand2), hand2)
+end
+
+function tocardnumJ(c)
+    if isdigit(c)
+        c - '0'
+    elseif c == 'T'
+        10
+    elseif c == 'J'
+        1
+    elseif c == 'Q'
+        11
+    elseif c == 'K'
+        12
+    elseif c == 'A'
+        13
+    else
+        throw("Illegal card!")
+    end
+end
+
+function find_hand_counts_J(hand)
+    counts = @MVector [0 for _ in 1:13]
+
+    for c in hand
+        counts[c] += 1
+    end
+
+    counts
+end
+
+function part2()
+    hands = Tuple{Int,SVector{5,Int},Int}[]
+
+    open("$(homedir())/aoc-input/2023/day7/input") do io
+        for l in eachline(io)
+            h, b = eachsplit(l)
+            hand = SVector((tocardnumJ(c) for c in h)...)
+            b = parse(Int, b)
+
+            push!(hands, (find_hand_type_J(hand), hand, b))
+        end
+    end
+
+    sort!(hands)
+
+    display(hands)
+
+    sum(b * i for (i, (_, _, b)) in enumerate(hands))
 end
