c0bc3a7b57371633bb8317c3d0f424c618f55b51
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-12-09 06:25:13 +0100

    recursive solution

--- src/day9.jl
+++ src/day9.jl
@@ -1,48 +1,30 @@
 
-function part1()
-    x = 0
-
-    open("$(homedir())/aoc-input/2023/day9/input") do io
-        for l in eachline(io)
-            nums = [parse.(Int, eachsplit(l))]
-
-            while !all(==(0), nums[end])
-                push!(nums, diff(nums[end]))
-            end
-
-            lastnums = [0]
-
-            for i in reverse(1:length(nums) - 1)
-                push!(lastnums, nums[i][end] + lastnums[end])
-            end
-
-            x += lastnums[end]
-        end
+function extrapolate(nums)
+    dn = diff(nums)
+    if all(==(0), dn)
+        last(nums)
+    else
+        last(nums) + extrapolate(dn)
     end
+end
 
-    x
+function part1()
+    open("$(homedir())/aoc-input/2023/day9/input") do io
+        sum(extrapolate(parse.(Int, eachsplit(l))) for l in eachline(io))
+    end
+end
+
+function extrapolate_back(nums)
+    dn = diff(nums)
+    if all(==(0), dn)
+        first(nums)
+    else
+        first(nums) - extrapolate_back(dn)
+    end
 end
 
 function part2()
-    x = 0
-
     open("$(homedir())/aoc-input/2023/day9/input") do io
-        for l in eachline(io)
-            nums = [parse.(Int, eachsplit(l))]
-
-            while !all(==(0), nums[end])
-                push!(nums, diff(nums[end]))
-            end
-
-            firstnums = [0]
-
-            for i in reverse(1:length(nums) - 1)
-                push!(firstnums, nums[i][1] - firstnums[end])
-            end
-
-            x += firstnums[end]
-        end
+        sum(extrapolate_back(parse.(Int, eachsplit(l))) for l in eachline(io))
     end
-
-    x
 end
